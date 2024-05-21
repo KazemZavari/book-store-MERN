6b05d10a537c45f9adfbadeb6cc044c524a6b04e
kazem <kazemzavari.69@gmail.com>
2024-05-21 23:29:25 +0330

    add notistack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import { router } from './routes/index.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index.jsx";
+import { SnackbarProvider } from "notistack";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SnackbarProvider>
+      <RouterProvider router={router} />
+    </SnackbarProvider>
   </React.StrictMode>
-
-)
+);
